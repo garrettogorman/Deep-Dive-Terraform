b51769efc20ce7c9fe4106a6b537bb037c6354c8
garrettogorman <garrettogorman@gmail.com>
2018-09-11 11:39:23 +0100

    Update backend.tf

--- module5/networking/backend.tf
+++ module5/networking/backend.tf
@@ -4,7 +4,7 @@
 terraform {
   backend "s3" {
     key            = "networking.state"
-    region         = "us-west-2"
-    dynamodb_table = "ddt-tfstatelock"
+    region         = "eu-west-1"
+    dynamodb_table = "gar-ddt-tfstatelock"
   }
 }
